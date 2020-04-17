b3f8bd9c3cd8c5d341649d7c68daafee90ad1127
siyamthandamkhonzana <62286288+siyamthandamkhonzana@users.noreply.github.com>
2020-04-17 08:18:16 +0100

    editing person file
    deleted commenting

---
+++ person.js
@@ -0,0 +1,17 @@
+class  Person{
+    constructor(name, age, gender, interest) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+        this.interest = interest;
+    }
+ hello() {
+           
+        console.log(`Hello,my name is ${this.name} and I am ${this.age} years old. My intrest are ${this.interest[0]}, ${this.interest[1]} and ${this.interest[2]}.`);
+    }
+        
+    }
+let person = new Person('Ryan',30 , 'male',['being a hardarse']// JavaScript:
+let person = new Person('Ryan',30,'male',['being a hardarse','agile', 'ssd hard drives'] )
+let greeting = person.hello()
+console.log(greeting)
